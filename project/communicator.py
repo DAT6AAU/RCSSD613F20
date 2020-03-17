39588910ae446438344dd136edd1682918aa4c06
judo347 <mikkelhvk@hotmail.com>
2020-03-17 17:05:38 +0100

    Fixed a few bugs in communicator.py

--- project/communicator.py
+++ project/communicator.py
@@ -1,29 +1,28 @@
 import listener
 import socket
 
-sock = socket
-
 
 def connect():
-    global sock
     # Split into function and other script - the sender
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     send_msg('(init Kuntz (version 7))', sock)
     send_msg('(move -15 0)', sock)
+    return sock
 
 
 def send_msg(msg, sock):
     sock.sendto(msg.encode(), 0, ('127.0.0.1', 6000))
 
 
-def recv_msg():
+def recv_msg(sock):
     data, addr = sock.recvfrom(1024)
     return data
 
 
 def main():
+    sock = connect()
     while True:
-        data = recv_msg()
+        data = recv_msg(sock)
         listener.parseSrvMsg(data.decode())
 
 
